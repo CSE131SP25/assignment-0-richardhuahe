1d8cc5537a54d0b44342a5621127d9d2b38ed6a3
richardhe <richardhe@localhost>
2025-01-22 13:16:56 -0600

    finished

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,7 +29,7 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+
 		robot.turnLeft(90);
 		robot.forward(23);
 		robot.turnRight(90);
